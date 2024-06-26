fad3cd05230336bc11141481ceb5b3ee160c4895
Diego Maroto <diego.diemg@gmail.com>
2024-06-26 10:33:15 +0200

    Added dynamodb memory for the bot on the main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@ variable "environment" {
   type = string
 }
 
+module "dynamodb_bot_memory" {
+  source                   = "./modules/dynamo_db"
+  prefix                   = "insanitydev"
+  name                     = "ai-bot-memory"
+  environment              = var.environment
+  hash_key                 = { name = "partition", type = "S" }
+  range_key                = { name = "id", type = "S" }
+}
+
 module "lambda_bot" {
   source                         = "./modules/lambda"
   prefix                         = "insanitydev"
@@ -12,7 +21,7 @@ module "lambda_bot" {
   handler                        = "lambda.handler"
   function_url                   = true
   reserved_concurrent_executions = 1
-  timeout = 10
+  timeout                        = 10
 }
 
 # module "api" {
